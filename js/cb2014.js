486172c0aa68be894d312b140fcb0b83b100ba18
Pedro Correia <pedro.a.correia@co.sapo.pt>
2013-10-07 20:30:04 +0100

    ...

---
+++ js/cb2014.js
@@ -0,0 +1,36 @@
+var snapper = new Snap({
+    element: document.getElementById('content'),
+    dragger: null,
+    disable: 'right',
+    addBodyClasses: true,
+    hyperextensible: false,
+    resistance: 0.5,
+    flickThreshold: 50,
+    transitionSpeed: 0.3,
+    easing: 'ease',
+    maxPosition: 266,
+    minPosition: -266,
+    tapToClose: true,
+    touchToDrag: true,
+    slideIntent: 40,
+    minDragDistance: 5
+});
+document.getElementById("snap-toggler").addEventListener('click', function(){
+    if( snapper.state().state=="left" ){
+        snapper.close();
+    } else {
+        snapper.open('left');
+    }
+});
+window.addEventListener('load',function(){
+    if (window.innerWidth >= 650) {
+        snapper.disable();
+    }
+});
+window.addEventListener('resize', function(){
+    if (window.innerWidth >= 650) {
+        snapper.disable();
+    } else {
+        snapper.enable();
+    }
+});

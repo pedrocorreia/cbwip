bdd1365c5295215dfe954965bf9f4fb8f78bebfb
Pedro Correia <pedro.a.correia@co.sapo.pt>
2013-10-07 18:51:29 +0100

    Adds skeleton html

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    folders: {
+      less: './less/',
+      css: './css/'
+    },
+
+    // COMPILES THE CSS
+    less: {
+      dev: {
+        files: {
+          '<%= folders.css %><%= pkg.name %>.css':['<%= folders.less %><%= pkg.name %>.less']
+        }
+      },
+
+      // COMPILES THE MINIFIED CSS
+      dist: {
+        options: {
+          yuicompress: true
+        },
+        files: {
+          '<%= folders.css %><%= pkg.name %>.min.css':['<%= folders.less %><%= pkg.name %>.less']
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['less/<%= pkg.name %>.less'],
+        tasks: ['less'],
+        options: {
+          spawn: false,
+        },
+      },
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['less']);
+  
+};
